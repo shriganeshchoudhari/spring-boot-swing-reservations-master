ec10079488555ecb2c249e5824127da10c8623c1
shri <shriganesha0@gmail.com>
2017-08-15 10:10:58 +0530

    set up

---
+++ spring-boot-swing-reservations/src/main/java/pl/dmichalski/spring_boot_swing_reservations/App.java
@@ -0,0 +1,13 @@
+package pl.dmichalski.spring_boot_swing_reservations;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
